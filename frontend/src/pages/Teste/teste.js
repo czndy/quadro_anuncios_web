5e2b0ff467008a7e24a7376df6f77805a5185957
czndy <guicozendey@gmail.com>
2022-12-16 16:53:20 -0300

    update Teste

--- frontend/src/pages/Teste/teste.js
+++ frontend/src/pages/Teste/teste.js
@@ -1,8 +1,26 @@
 import Dashboard from "../Dashboard/dashboard";
 import Menu from "../Menu/menu"
+import { Box } from "@mui/material";
 
 export default function Teste(){
     return(
-        <Menu/>
+        <Box sx={{display: "flex", height: "100vh", width:"100%"}}>
+            <Menu/>
+            <Box
+                sx={{
+                "& > :not(style)": {
+                    m: "auto",
+                    width: 600,
+                    height: 600,
+                },
+                }}
+                m="auto"
+                display="flex"
+                alignItems="center"
+                width="1000px"
+                height="98vh"
+            >
+            </Box>
+        </Box>
     )
 }
